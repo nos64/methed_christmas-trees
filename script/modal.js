81e7dbb7cfef2265cfa5da3dc7811455db3d64dd
Mikhail Nosov <manosov1984@gmail.com>
2021-12-24 16:21:54 +0400

    add timer to new year

--- script/modal.js
+++ script/modal.js
@@ -9,8 +9,6 @@ buttonsOrder.forEach(buttonOrder => {
   })
 });
 
-console.log(buttonsOrder)
-console.log('overlayOrder: ', overlayOrder);
 
 overlayOrder.addEventListener('click', event => {
   const target = event.target;
